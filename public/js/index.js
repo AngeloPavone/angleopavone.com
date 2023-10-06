9dc2443d9e7e412641362571f41b07e5ca0bccb9
AngeloPavone <pavone@gmx.us>
2023-10-06 14:24:33 -0700

    removed extra posts from db and created two simple ones with a connection from foo -> bar for testing

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,11 @@ import { Node } from './graph.js';
 fetch('/api/blogposts')
   .then((res) => res.json())
   .then((data) => {
-    data.forEach((post, index) => {
-      new Node(post.title);
+    data.forEach((post) => {
+    console.log(post.connections);
+    new Node(post.title);
     });
   })
   .catch((error) => {
-    console.error('Error fetching data:', error);
+    console.error('Error fetching blog posts:', error);
   });
